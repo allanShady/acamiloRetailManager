67d69a35ed0ea2b4eee23d3cc257696699ef18df
Allan Camilo <acamilo.ag@gmail.com>
2019-02-09 09:23:30 +0200

    Adds container and to Bootstrapper.
    Added a simple container to handler the intation of all classes.

--- ACRMDesktopUI/Bootstrapper.cs
+++ ACRMDesktopUI/Bootstrapper.cs
@@ -11,6 +11,24 @@ namespace ACRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        SimpleContainer _container = new SimpleContainer();
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.Name, viewModelType));
+        }
+
         public Bootstrapper()
         {
             Initialize();
@@ -19,6 +37,21 @@ namespace ACRMDesktopUI
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance); 
         }
     }
 }
